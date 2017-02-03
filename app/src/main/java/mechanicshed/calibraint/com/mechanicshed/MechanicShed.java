b07fffb924994cb89e8fe4982171d58b9d18b93e
karthikpks <karthikparameswarankpm@gmail.com>
2017-02-03 15:48:56 +0530

    first initial code base commit

---
+++ app/src/main/java/mechanicshed/calibraint/com/mechanicshed/MechanicShed.java
@@ -0,0 +1,13 @@
+package mechanicshed.calibraint.com.mechanicshed;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MechanicShed extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mechanic_shed);
+    }
+}
