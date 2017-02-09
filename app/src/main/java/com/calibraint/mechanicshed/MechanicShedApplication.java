d8929b6ce6a874940840e5af763ff96814d131d2
Aruljothi K <jak.jothi@gmail.com>
2017-02-09 16:41:36 +0530

    develop initial commit

---
+++ app/src/main/java/com/calibraint/mechanicshed/MechanicShedApplication.java
@@ -0,0 +1,11 @@
+package com.calibraint.mechanicshed;
+
+import android.support.multidex.MultiDexApplication;
+
+/**
+ * Created by karthik on 2/9/17.
+ */
+
+public class MechanicShedApplication extends MultiDexApplication {
+    
+}
---
+++ app/src/main/java/com/calibraint/mechanicshed/SplashScreenActivity.java
@@ -0,0 +1,41 @@
+package com.calibraint.mechanicshed;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Thread splashTread;
+
+        getSupportActionBar().hide();
+        //Start MainActivity
+        splashTread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    int waited = 0;
+                    // Splash screen pause time
+                    while (waited < 3500) {
+                        sleep(100);
+                        waited += 100;
+                    }
+                    Intent intent = new Intent(SplashScreenActivity.this,
+                            MechanicShedActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    startActivity(intent);
+                    SplashScreenActivity.this.finish();
+                } catch (InterruptedException e) {
+                    // do nothing
+                } finally {
+                    SplashScreenActivity.this.finish();
+                }
+
+            }
+        };
+        splashTread.start();
+    }
+}
